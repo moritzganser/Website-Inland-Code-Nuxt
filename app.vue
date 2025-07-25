f738239082c19782cefbcc5af7750bddea63b6c0
moritzgansergit config --global user.email moritz.ganser.email@gmail.com <moritzganser@MBP-von-Moritz.fritz.box>
2025-07-25 18:30:02 +0200

    Initialer Commit

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div>
+    <div id="navbar">
+      <h1>Inland Code</h1>
+      <button id="navbar_button">Home</button>
+      <div class="bg-black animate-ping">1</div>
+    </div>
+
+
+
+  </div>
+
+</template>
+<style>
+#navbar{
+  background-color: rgb(0, 0, 156);
+}
+#navbar_button{
+  background-color: rgb(49, 49, 255);
+  color: white;
+  padding: 10px;
+  border-radius: 15px;
+}
+</style>
